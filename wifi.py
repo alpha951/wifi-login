b08b18a5b4a90195ca515aae39aa225bfd16802f
Keshav Carpenter <20uec068@lnmiit.ac.in>
2023-06-12 15:34:32 +0530

    reduce wait time to load page

--- wifi.py
+++ wifi.py
@@ -10,23 +10,28 @@ options.add_argument('--ignore-certificate-errors')
 # Specify the path to the ChromeDriver executable or left blank if already added in PATH variable
 # driver_path = 'C:/Users/Keshav/Desktop/chromedriver_win32/chromedriver.exe'
 
-driver = webdriver.Chrome( options=options)
+driver = webdriver.Chrome(options=options)
+
 
 def login(username, password):
     driver.get('https://172.22.2.6/connect/PortalMain')
-    wait = WebDriverWait(driver, 10)
-    
+    wait = WebDriverWait(driver, 4)
+
     # Wait for the username field to be present
-    username_field = wait.until(EC.presence_of_element_located((By.ID, 'LoginUserPassword_auth_username')))
+    username_field = wait.until(EC.presence_of_element_located(
+        (By.ID, 'LoginUserPassword_auth_username')))
     username_field.send_keys(username)
 
     # Wait for the password field to be present
-    password_field = wait.until(EC.presence_of_element_located((By.ID, 'LoginUserPassword_auth_password')))
+    password_field = wait.until(EC.presence_of_element_located(
+        (By.ID, 'LoginUserPassword_auth_password')))
     password_field.send_keys(password)
 
     # Submit the login form
-    login_button = wait.until(EC.element_to_be_clickable((By.ID, 'UserCheck_Login_Button')))
+    login_button = wait.until(EC.element_to_be_clickable(
+        (By.ID, 'UserCheck_Login_Button')))
     login_button.click()
+
 
 login('20uec068', 'password')  # Replace with your username and password
 
